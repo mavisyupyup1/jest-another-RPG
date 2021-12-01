59f33d7a53e187c261a037886976797c434482a2
Your Name <graceliu@gracelius-iMac.home>
2021-12-01 11:22:46 -0500

    enemy object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,4 +1,9 @@
 const Potion=require('../lib/Potion.js')
+test('create a random potion object',()=>{
+    const potion = new Potion('health');
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
 test ('create a health potion object',()=>{
     const potion = new Potion();
     expect(potion.name).toEqual(expect.any(String));
